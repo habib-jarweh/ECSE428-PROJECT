b538cbd2f6a43e55656ef70b43146c4a6fa54567
Habib Jarweh <habibjarweh@habibs-mbp.home>
2024-02-18 11:32:27 -0500

    create folder

---
+++ backend/src/test/java/ecse428/backend/acceptance/CucumberFeaturesTestRunner.java
@@ -0,0 +1,10 @@
+package ecse428.backend.acceptance;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = "pretty", features = "src/test/resources/")
+public class CucumberFeaturesTestRunner {
+}
