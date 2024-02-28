cd65ab02cddce3694f8fe249a4c4c1af2e4ba3f4
Matthieuhakim <77667703+Matthieuhakim@users.noreply.github.com>
2024-02-27 21:24:59 -0500

    Fix Dashboard restrictions

--- backend/src/main/java/ecse428/backend/controller/CustomerController.java
+++ backend/src/main/java/ecse428/backend/controller/CustomerController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.stream.Collectors;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/customers")
 public class CustomerController {
 
