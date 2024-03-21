e4f34e50559d5bc5fda66f628713f6a9bdbdefa8
Matthieuhakim <77667703+Matthieuhakim@users.noreply.github.com>
2024-03-21 17:01:51 -0400

    Added service methods

---
+++ backend/src/main/java/ecse428/backend/dto/ReviewDto.java
@@ -0,0 +1,67 @@
+package ecse428.backend.dto;
+
+import jakarta.validation.constraints.NotNull;
+
+public class ReviewDto {
+
+    @NotNull
+    private Long reviewID;
+
+    @NotNull
+    private Long orderID;
+
+    @NotNull
+    private String mealName;
+
+    private String reviewText;
+
+    private Integer rating;
+
+    public ReviewDto(Long reviewID, Long orderID, String mealName, String reviewText, Integer rating) {
+        this.reviewID = reviewID;
+        this.orderID = orderID;
+        this.mealName = mealName;
+        this.reviewText = reviewText;
+        this.rating = rating;
+    }
+
+    public Long getReviewID() {
+        return reviewID;
+    }
+
+    public void setReviewID(Long reviewID) {
+        this.reviewID = reviewID;
+    }
+
+    public Long getOrderID() {
+        return orderID;
+    }
+
+    public void setOrderID(Long orderID) {
+        this.orderID = orderID;
+    }
+
+    public String getMealName() {
+        return mealName;
+    }
+
+    public void setMealName(String mealName) {
+        this.mealName = mealName;
+    }
+
+    public String getReviewText() {
+        return reviewText;
+    }
+
+    public void setReviewText(String reviewText) {
+        this.reviewText = reviewText;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+}
