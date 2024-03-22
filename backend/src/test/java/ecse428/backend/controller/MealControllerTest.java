8b9614b4752c166d6e4fccb8e9b4b80cf01b9361
habib-jarweh <habib.jarweh@gmail.com>
2024-03-22 11:51:41 -0400

    remove unused imports

--- backend/src/test/java/ecse428/backend/controller/MealControllerTest.java
+++ backend/src/test/java/ecse428/backend/controller/MealControllerTest.java
@@ -1,10 +1,6 @@
 package ecse428.backend.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import ecse428.backend.dto.CustomerDto;
-import ecse428.backend.model.Customer;
-import ecse428.backend.model.SmartEats.DietaryRestriction;
-import ecse428.backend.service.CustomerService;
 import ecse428.backend.service.MealService;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -19,15 +15,8 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.validation.BeanPropertyBindingResult;
-import org.springframework.validation.BindingResult;
 import static org.mockito.BDDMockito.given;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyDouble;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
