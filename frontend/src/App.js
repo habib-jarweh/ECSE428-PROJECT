5e6fe969202b234dd97a535f941fa56d419e2170
habib-jarweh <habib.jarweh@gmail.com>
2024-03-28 14:14:51 -0400

    admin view and delete customers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import HomePage from './components/HomePage';
 import SignIn from './components/user/SignIn'; 
 import SignUp from './components/user/SignUp'; 
 import CustomerPage from './components/customer/CustomerPage';
+import AdminPage from './components/admin/AdminPage';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/customer" element={<CustomerPage />} />
+        <Route path="/admin" element={<AdminPage />} />
         {/* Add more routes as needed */}
       </Routes>
     </Router>
